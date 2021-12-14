2184d82579177582809d1c5bda46bbf6f3b3b39a
kevin <jhkevin21@gmail.com>
2021-12-14 14:22:16 -0700

    Completed login function with reactive form

--- src/app/pages/login-screen/login-screen.module.ts
+++ src/app/pages/login-screen/login-screen.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { LoginScreenPage } from './login-screen.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginScreenPageRoutingModule
+    LoginScreenPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginScreenPage]
 })
-export class LoginScreenPageModule {}
+export class LoginScreenPageModule { }
--- src/app/pages/login-screen/login-screen.page.ts
+++ src/app/pages/login-screen/login-screen.page.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.page.html',
   styleUrls: ['./login-screen.page.scss'],
 })
-export class LoginScreenPage implements OnInit {
+export class LoginScreenPage implements OnInit, DoCheck {
+  validationFormUser: FormGroup;
 
-  constructor() { }
+  validationUserMessage = {
+    email: [
+      { type: 'required', message: 'Please enter your Email' },
+      { type: 'pattern', message: 'The Email entered is Incorrect.Try again' }
+    ],
+    password: [
+      { type: 'required', message: 'Please enter your password!' },
+      { type: 'minlength', message: 'The password must be at least 5 characters or more' }
+    ]
+  };
+
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validationFormUser = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ]))
+    });
+  }
+
+  ngDoCheck(): void {
+    console.log(this.validationFormUser);
+    console.log('email', this.validationFormUser.get('email'))
+    console.log('password', this.validationFormUser.get('password'))
+    // console.log('password error', this.validationFormUser.get('password').hasError);
+    console.log('password error', this.validationFormUser.get('password').hasError('minlength'));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  LoginUser(value) {
+    console.log("Am logged in")
   }
 
 }
